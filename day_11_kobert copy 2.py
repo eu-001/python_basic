a76434a6cd7a568f6dbfbbc822d83e4c7893c624
eu-001 <chcolatei71@gmail.com>
2025-06-26 15:25:24 +0900

    -

--- day_11_kobert copy 2.py
+++ day_11_kobert copy 2.py
@@ -11,6 +11,9 @@ saved_embeddings = data["embeddings"]
 saved_ids = data["ids"]
 saved_questions = data["questions"]
 
-print(saved_embeddings)
-print(saved_ids)
-print(saved_questions)
+cos_sim= util.pytorch_cos_sim(user_embedding, saved_embeddings)[0]
+top_k=torch.topk(cos_sim, k=1)
+top_idx=top_k.indices[0].item()
+print(f"가장 유사한 질문:{saved_questions[top_idx]}")
+print(f"DB ID:{saved_ids[top_idx]}")
+print(f"유사도 점수:{top_k.values[0].item()}")

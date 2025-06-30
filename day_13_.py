b20532ac06ed34f4ca6426a4e0e67f50149d2c4e
eu-001 <chcolatei71@gmail.com>
2025-06-30 14:27:09 +0900

    00

--- day_13_.py
+++ day_13_.py
@@ -16,3 +16,24 @@ mydataset = {
 myvar = pandas.DataFrame(mydataset)
 
 print(myvar)
+
+import pandas as pd
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+df = pd.DataFrame(data, index = ["day1", "day2", "day3"])
+
+print(df) 
+
+#ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+data1 = {
+    "집값" : [30, 15, 7]
+}
+
+dt1 = pd.DataFrame (data1, index = [2022, 2017, 2010])
+
+print(dt1)

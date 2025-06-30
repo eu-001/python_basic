9bc789622a8e1af4f0b50088b1f75f39b428d624
eu-001 <chcolatei71@gmail.com>
2025-06-30 13:38:32 +0900

    00

--- day_13_.py
+++ day_13_.py
@@ -1,13 +1,17 @@
 import pandas
 
 '''
-dictionary: 무조건 {} 나오고 안에 {'키'} 나오고 {'키': 값 } 이런형태를 가지고 있디
+dictionary: 무조건 {} 나오고 안에 {'키'} 나오고 {'키': 값 } 이런형태를 가지고 있다
+값은 무조건 list여야 한다. 각 리스트 갯수도 서로 같아야한다.
+이 자료구조 법칙을 어기면 함수 작동을 안한다
 '''
 
 mydataset = {
   'cars': ["BMW", "Volvo", "Ford"],
   'passings': [3, 7, 2]
 }
+
+# dictoionary를  pandas로 읽는 건, 무조건  pandas.DataFrame(mydataset)이 코드로 정해져 있다.
 
 myvar = pandas.DataFrame(mydataset)
 

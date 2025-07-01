adc53734035313985ff6e634e649ec0259782666
eu-001 <chcolatei71@gmail.com>
2025-07-01 13:49:04 +0900

    00

---
+++ day_14.py
@@ -0,0 +1,24 @@
+#matplot - png로 그래프 보여주기. 실시간 x
+
+"""
+ import matplotlib.pyplot as plt
+ import numpy as np
+
+ xpoints = np.array([0, 6])
+ ypoints = np.array([0, 250])
+
+ plt.plot(xpoints, ypoints)
+ plt.show()
+"""
+#수학으로 그래프 해석하면 (0,0)(6,250) 걍 기하하는 건디
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+xpoints = np.array([0.3, 0.7, 2.1, 2.2])
+plt.plot(ypoints, marker = 'o')
+plt.plot(xpoints, linestyle = 'dotted')
+plt.ylabel("omg")
+plt.xlabel("what")
+plt.show()

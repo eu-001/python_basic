512e8c78a8b52a56e64dede29bab3f761d507eca
eu-001 <chcolatei71@gmail.com>
2025-07-02 15:06:45 +0900

    55

--- day_15_ copy.py
+++ day_15_ copy.py
@@ -3,22 +3,37 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-df=pd.read_csv("car_gasoline.csv") #내가 찾던 어떤 줄인말인지 머시기...
+# df=pd.read_csv("car_gasoline.csv") #내가 찾던 어떤 줄인말인지 머시기...
 
-x=df[["weight","engine_size"]].values
-y=df["fuel_efficiency"].values
-print(x)
+# x=df[["weight","engine_size"]].values
+# y=df["fuel_efficiency"].values
+# print(x)
+
+# model=LinearRegression()
+# model.fit(x,y)
+
+# weight_input=1500
+# engine_input=2.0
+
+# x_input=np.array([weight_input,engine_input]).reshape(1,-1)
+# predicted_fuel=model.predict(x_input)[0]
+# print(f"예상 연비: 1리터로 {predicted_fuel}km 갈 수 있습니다")
+
+house_size=np.array([30, 40, 50, 60, 70, 80, 90, 100])
+np.random.seed(42) 
+price = house_size * 50 + np.random.randint(-200, 200, size=house_size.shape)
+
+print("집 크기:", house_size)
+print("가격:", price)
 
 model=LinearRegression()
-model.fit(x,y)
+model.fit(house_size.reshape(-1, 1), price)
 
-weight_input=1500
-engine_input=2.0
+my_houze_size=33
 
-x_input=np.array([weight_input,engine_input]).reshape(1,-1)
-predicted_fuel=model.predict(x_input)[0]
-print(f"예상 연비: 1리터로 {predicted_fuel}km 갈 수 있습니다")
-
+x_input = np.array([[33]])
+predicted = model.predict(x_input)[0]
+print(f"내 집 가격:{predicted}")
 
 
 

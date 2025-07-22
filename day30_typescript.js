66c6c33e970b6090e71f03b3e4fa81eaa5b3b173
eu-001 <chcolatei71@gmail.com>
2025-07-22 15:50:46 +0900

    00

---
+++ day30_typescript.js
@@ -0,0 +1,32 @@
+// let test1 = "string 이에요"
+// let test2: string = "저도 string 이에요"
+// //console.log(`test1:${test1}`)*/
+// let number1 = 1
+// let number2: number =2
+// let bool1 = true
+// let bool2: boolean = false
+// let array1: string[] = []
+// array1.push("sdsd")
+// let array2: number[] = []
+// array2.push(1)
+// //console.log(`array2:${array2}`)
+// let jsonObject1 = {
+//     number1:1
+//     , string1:"fff"
+//     , bool1:true
+// }
+// console.log(`string1:${"string1"}`)
+// console.log(`number1:${number1}`)
+// function add_test1(){
+//     const a = 10;
+//     const b = 30;
+//     return a+b ;
+// }
+// console.log(`add_test1:${add_test1()}`)
+// 이건 자바임 ㅁㅊ
+function add_test1() {
+    var a = 10;
+    var b = 30;
+    return a + b;
+}
+console.log("add_test1: ".concat(add_test1()));
---
+++ day30_typescript.ts
@@ -0,0 +1,45 @@
+// let test1 = "string 이에요"
+
+// let test2: string = "저도 string 이에요"
+// //console.log(`test1:${test1}`)*/
+
+
+// let number1 = 1
+// let number2: number =2
+
+// let bool1 = true
+// let bool2: boolean = false
+
+// let array1: string[] = []
+// array1.push("sdsd")
+
+// let array2: number[] = []
+// array2.push(1)
+// //console.log(`array2:${array2}`)
+
+// let jsonObject1 = {
+//     number1:1
+//     , string1:"fff"
+//     , bool1:true
+// }
+
+// console.log(`string1:${"string1"}`)
+// console.log(`number1:${number1}`)
+
+// function add_test1(){
+//     const a = 10;
+//     const b = 30;
+//     return a+b ;
+    
+// }
+
+// console.log(`add_test1:${add_test1()}`)
+// 이건 자바임 ㅁㅊ
+
+function add_test1(): number {
+    const a: number = 10;
+    const b: number = 30;
+    return a + b;
+}
+
+console.log(`add_test1: ${add_test1()}`);
